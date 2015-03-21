b095363a0ca58c95bf4c7db3956c6307b41173be
Chris Griffiths <calufrax@yahoo.com>
2015-03-21 23:27:15 +1000

    Increase comments, didn't change code

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function caches a the inverse of a given matrix so that 
+## - if the matrix has not yet been inverted, calculate and store inverse
+## - if the matrix has been inverted, retrieve and send precalculated inverse
+## Assumptions:
+##    - matrix is invertible
+##    - matrix does not change between uses of these functions.
 
 ## create list containing functions to set and get Inverse
+## first try, based on example code 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         setInv <- function(y){
@@ -13,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## check if it's been solved, if not then set it
-
+## first try, based on example code
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m<-x$getInv()
