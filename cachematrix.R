6bed73424e05a56436ebea46f5c8ba2e156cc965
Chris Griffiths <calufrax@yahoo.com>
2015-03-21 23:34:58 +1000

    More comments into code

--- cachematrix.R
+++ cachematrix.R
@@ -9,23 +9,33 @@
 ## first try, based on example code 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+
+        ##function to set the inverse
         setInv <- function(y){
             x<<-y
             m<<-NULL
         }
+ 
+        ## function to return inverse
         getInv<-function() x
+
         list(setInv = setInv, getInv = getInv)
 }
+
+
 
 ## check if it's been solved, if not then set it
 ## first try, based on example code
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## First check for precalculated inverse
         m<-x$getInv()
         if(!is.null(m)) {
+                ## precalculated inverse in cache, so return it and exit
                 message("getting cached data")
                 return(m)
         }
+        ## if not precalculated, calculate, set, and return value.
         m <- solve(x)
         x$setInv(m)
         m
